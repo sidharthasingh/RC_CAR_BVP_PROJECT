4965712ebb6f68d091ba9cf2a2935ed63d308790
Sidhartha Singh <15666676+sidharthasingh@users.noreply.github.com>
2017-10-11 09:54:53 +0530

    New sketch

--- Arduino/DEVIL_S_LESSON__BVP_PROJECT_/DEVIL_S_LESSON__BVP_PROJECT_.ino
+++ Arduino/DEVIL_S_LESSON__BVP_PROJECT_/DEVIL_S_LESSON__BVP_PROJECT_.ino
@@ -1,10 +1,14 @@
 #include<Servo.h>
 #include<SoftwareSerial.h>
 
-#define SERVO_PIN 10 // The digital pin to which the Servo is attached
+#define SERVO_PIN 5 // The digital pin to which the Servo is attached
 #define BT_PINTX 4 // The digital pin to which the HC05 TX module is connected - TX
 #define BT_PINRX 5 // The digital pin to which the HC05 RX module is connected - RX
 #define BT_BAUD_RATE  115200 // The baud rate at which the HC05 module is operating. Default is 9600 (May be :-P)
+#define IN_1 = 6 ;
+#define IN_2 = 7 ;
+
+
 /*
   DIRECTIONAL LOGIC
   the 'dir' variable is for the direction being received
@@ -19,13 +23,15 @@
   7 : West
   8 : North-west
 */
+
 /* The global variable that have been defined for the whole code*/
 int throttle; // Decides the speed of the motors
 int dir; // The direction in which the car has to move (The angles for the servo)
 unsigned char val_received;
+int SPEED = 0;
 
 SoftwareSerial BT(BT_PINTX, BT_PINRX); // The BT object. A reference to the HC05 Serial module
-Servo serv; // This is the motor connected to the steering
+Servo myservo; // This is the motor connected to the steering
 
 void refresh(); // This function re-syncs the global values to the whole system
 
@@ -33,8 +39,8 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Setup() called");
 
-  serv.attach(SERVO_PIN);
-   BT.begin(BT_BAUD_RATE);
+  myservo.attach(SERVO_PIN);
+  BT.begin(BT_BAUD_RATE);
   
   Serial.println("Setup() end");
 }
@@ -61,7 +67,94 @@ void loop() {
   }
 }
 
+void stop();
+void front();
+void front_right();
+void right();
+void back_right();
+void back();
+void back_left();
+void left();
+void front_left();
+
+void setDirections()
+{
+  switch(dir)
+  {
+    case 0:stop();break;
+    case 1:front();break;
+    case 2:front_right();break;
+    case 3:right();break;
+    case 4:back_right();break;
+    case 5:back();break;
+    case 6:back_left();break;
+    case 7:left();break;
+    case 8:front_left();break;
+  }
+}
+
+void setSpeed()
+{
+  // Code to set the speed using the "val_received" variable
+  SPEED = (throttle*255)/100;
+}
+
 void refresh()
 {
-  
+  setSpeed();
+  setDirections();
+}
+
+void stop(){
+  myservo.write(90);
+  digitalWrite(IN_1,LOW) ;
+  digitalWrite(IN_2,LOW) ;
+}
+
+void front(){
+  myservo.write(90);
+  digitalWrite(IN_1,SPEED) ;
+  digitalWrite(IN_2,LOW) ;
+}
+
+void front_right(){
+  myservo.write(135);
+  digitalWrite(IN_1,SPEED) ;
+  digitalWrite(IN_2,LOW) ;
+}
+
+void right(){
+  myservo.write(180);
+  digitalWrite(IN_1,SPEED) ;
+  digitalWrite(IN_2,LOW) ;
+}
+
+void back_right(){
+  myservo.write(135);
+  digitalWrite(IN_1,LOW) ;
+  digitalWrite(IN_2,SPEED) ;
+}
+
+void back(){
+  myservo.write(90);
+  digitalWrite(IN_1,LOW) ;
+  digitalWrite(IN_2,SPEED) ;
+}
+
+void back_left(){
+  myservo.write(45);
+  digitalWrite(IN_1,LOW) ;
+  digitalWrite(IN_2,SPEED) ;
+}
+
+void left(){
+  myservo.write(0);
+  digitalWrite(IN_1,SPEED) ;
+  digitalWrite(IN_2,LOW) ;
+}
+
+void front_left(){
+  myservo.write(45);
+  digitalWrite(IN_1,SPEED) ;
+  digitalWrite(IN_2,LOW) ;
 }
